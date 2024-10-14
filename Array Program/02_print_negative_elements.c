93bc929455cc63eb00d9cdbfcb13846f81465c29
Ankita Das <adas22156@gmail.com>
2024-10-15 01:06:05 +0530

    program

--- Array Program/02_print_negative_elements.c
+++ Array Program/02_print_negative_elements.c
@@ -2,7 +2,7 @@
 
 void main ()
 {
-    int n,i,arr[100];
+    int n,i,arr[100],count=0;
     printf("Enter the elements:");
     scanf("%d",&n);
     printf("Enter %d elements:",n);
@@ -14,6 +14,9 @@ void main ()
     for(i=0; i<n; i++)
     {
         if(arr[i] < 0)
-        printf("%d\t",arr[i]);
+        {printf("%d\t",arr[i]);
+        count++;
+        }
     }
+    printf("\nNumber of  negetive elements: %d",count);
 }
---
+++ Array Program/08_insert_element.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,arr[100],i,j,location;
+
+    printf("Enter the size ofarray: ");
+    scanf("%d",&n);
+    printf("Enter the %d elements: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+
+}
