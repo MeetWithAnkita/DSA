46188bd04661134f414e45b295a8038ae415bde8
Ankita Das <adas22156@gmail.com>
2024-10-18 23:43:21 +0530

    hii

---
+++ Array Program/11_unique_element.c
@@ -0,0 +1,15 @@
+// Unique element means frequency of element is 1 
+#include<stdio.h> 
+void main()
+{
+    int arr[100],n,i  ;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    printf("Enter the elementrs:");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+
+}
