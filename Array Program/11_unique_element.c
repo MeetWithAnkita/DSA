53d0205d167b4518249056bbfd808b51dfbcf562
Ankita Das <adas22156@gmail.com>
2024-10-19 17:46:00 +0530

    17:45

--- Array Program/11_unique_element.c
+++ Array Program/11_unique_element.c
@@ -2,7 +2,7 @@
 #include<stdio.h> 
 void main()
 {
-    int arr[100],n,i  ;
+    int arr[100],n,i,j,count ,frequency[100],visited = -1  ;
     printf("Enter the size of array:");
     scanf("%d",&n);
     printf("Enter the elementrs:");
@@ -11,5 +11,39 @@ void main()
         scanf("%d",&arr[i]);
     }
     
+    for (i=0;i<n;i++)
+    {
+        count=1;
+        if (frequency[i] == visited)
+        {
+            continue;
+        }
+        for ( j = i+1; j < n; j++)
+        {
+            if (arr[i] == arr[j] )
+            {
+                count++;
+                frequency[j] = visited;
+            }
+            frequency[i] = count;
+        }        
+    }
+
+    for(i=0; i<n; i++)
+    {
+        if (frequency[i] != visited)
+        {
+            printf("%d repeated %d times.\n",arr[i],frequency[i]);
+        }
+    }
+
+    printf("Unique numbers are :");
+    for (i=0 ; i<n ; i++)
+    {
+        if (frequency[i] == 1 )
+        {
+            printf("%d\t",arr[i]);   
+        }
+    }
 
 }
