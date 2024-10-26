3eef9b9c6c9b0fcc7d3cf855ec8563bcedf99913
Ankita Das <adas22156@gmail.com>
2024-10-26 20:22:59 +0530

    26/10/24

--- Matrix Program/11_check_upper_triangle_matrix_and_its_sum.c
+++ Matrix Program/11_check_upper_triangle_matrix_and_its_sum.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-    int arr[100][100],i,j,r,c,isupper=0 ;
+    int arr[100][100],i,j,r,c,isupper=0,sum=0 ;
 
     printf("Enter the row and column of a matrix :");
     scanf("%d%d",&r,&c);
@@ -13,7 +13,7 @@ void main()
             scanf("%d",&arr[i][j]);
         }
     }
-    printf("%d * %d matrix is:\n");
+    printf("%d * %d matrix is:\n",r,c);
     for(i=0; i<r; i++)
     {
         for(j=0;j<c;j++)
@@ -31,6 +31,7 @@ void main()
         for(j=0; j<c; j++)
         {
             if((i>j) & arr[i][j]==0)
+            // An upper triangular matrix is a square matrix where all the entries below the main diagonal are zero
             {
                 isupper=1;
             }
@@ -39,6 +40,19 @@ void main()
     if (isupper==1)
     {
         printf("The matrix is upper triangle.");
+        // sum of upper triangular matrix
+        for(i=0;i<r;i++)
+        {
+            for(j=0;j<c;j++)
+            {
+                if((i<j) && (arr[i][j]!=0))
+                {
+                    sum += arr[i][j];
+                }
+            }
+        }
+        printf("\nSummetion of upper triangular matrix:%d",sum);
+
     }
     else{
         printf("This is not upper triangle matrix.");
